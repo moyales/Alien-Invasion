445c5e3353cba7baea42e7d7ecdfe7ae883c7c87
Matthew Oyales <moyales@stevens.edu>
2019-12-16 23:10:43 -0500

    Moved games settings to new module

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    '''A class to store the settings for Alien Invasion.'''
+
+    def __init__(self):
+        '''Initialise all the of the game's settings.'''
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
