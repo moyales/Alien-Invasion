a221f910c8c03f497ceb5f3f5f5b7981b6ddb54f
Matthew Oyales <moyales@stevens.edu>
2019-12-17 00:27:47 -0500

    basic left and right movement

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ class Settings():
     def __init__(self):
         '''Initialise all the of the game's settings.'''
         # Screen Settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (51, 204, 255)
+        self.screen_width = 1200            # In pixels
+        self.screen_height = 750
+        self.bg_color = (51, 204, 255)      # RGB colors
+
+        # Ship Settings
+        self.ship_speed_factor = 0.8
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         '''Initialise the ship and its starting position'''
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('C:/Users/matto/Documents/Programming/Python_Work/alien_invasion/images/ship.bmp')
@@ -14,6 +15,24 @@ class Ship():
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''Update the ship's position based on the movement flags.'''
+        # Update center value, not the rect
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         '''draw the ship at its current location'''
