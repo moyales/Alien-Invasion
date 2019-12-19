bf917a51b08135c6982e62fa8b513015dd1e4df0
Matthew Oyales <moyales@stevens.edu>
2019-12-19 11:24:32 -0500

    Collisions with aliens and bullets

--- game_functions.py
+++ game_functions.py
@@ -67,7 +67,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     '''Update position of bullets and get rid of old bullts.'''
     # Update bullet positions.
     bullets.update()
@@ -76,6 +76,18 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    '''Respond to bullet-alien collissions.'''
+    # Remove any bullets and aliens that have collided.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
     '''Determine the number of aliens that fit in a row.'''
